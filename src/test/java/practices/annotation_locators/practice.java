2eb2c6975a56cd8aa8237877ef7c28e3516d3576
Muhammet Çakmak <muhammetckmk46@gmail.com>
2025-01-24 17:09:05 +0300

    seleniumPractice

--- src/test/java/practices/annotation_locators/practice.java
+++ src/test/java/practices/annotation_locators/practice.java
@@ -1,4 +1,35 @@
 package practices.annotation_locators;
 
 public class practice {
+    // 1-) Senaryo: Google ana sayfasını açın, arama kutusunu locate edin ve görünür olduğunu doğrulayın.
+    //Locator Yöntemi: name
+
+    // 2-) Senaryo: Amazon ana sayfasını açın, "Ara" butonunu locate edin ve tıklanabilir olduğunu doğrulayın.
+    //Locator Yöntemi: id
+
+    // 3-) Senaryo: Facebook giriş sayfasını açın, "Giriş Yap" butonunu locate edin ve metninin "Giriş Yap" olduğunu doğrulayın.
+    //Locator Yöntemi: name
+
+    // 4-)Senaryo: Hepsiburada ana sayfasını açın, "Kategoriler" menüsünü locate edin ve görünür olduğunu doğrulayın.
+    //Locator Yöntemi: cssSelector
+
+    // 5-)Senaryo: Twitter giriş sayfasını açın, şifre alanını locate edin ve içeriğinin boş olduğunu doğrulayın.
+    //Locator Yöntemi: xpath
+
+
+    // 6-)Senaryo: YouTube ana sayfasını açın, arama kutusunu locate edin ve placeholder metninin "Search" olduğunu doğrulayın.
+    //Locator Yöntemi: name
+
+    // 7-)Senaryo: Trendyol ana sayfasını açın, "Sepetim" butonunu locate edin ve görünür olduğunu doğrulayın.
+    //Locator Yöntemi: cssSelector
+
+    // 8-)Senaryo: LinkedIn giriş sayfasını açın, şifre giriş alanını locate edin ve etkin olduğunu doğrulayın.
+    //Locator Yöntemi: cssSelector
+
+    // 9-)Senaryo: GitHub ana sayfasını açın, "Sign up for GitHub" linkini locate edin ve href özelliğinin "https://github.com/signup" olduğunu doğrulayın.
+    //Locator Yöntemi: xpath
+
+    // 10-) Senaryo: Instagram giriş sayfasını açın, kullanıcı adı giriş alanını locate edin ve görünür olduğunu doğrulayın.
+    //Locator Yöntemi: cssSelector
+
 }
